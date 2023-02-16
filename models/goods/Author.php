5781192703fa6e48e174ce62b4a188e1d863f354
Abhishek Sharma <102028325+Abhi2003shek@users.noreply.github.com>
2023-02-16 18:40:47 +0530

    Update Author.php

--- models/goods/Author.php
+++ models/goods/Author.php
@@ -1,8 +1,7 @@
 <?php
 
-spl_autoload_register(/**
- * @param $class
- */ function ($class){
+spl_autoload_register(
+    function ($class){
     $arr=['goods','interfaces','orders','reviews','serve'];
     foreach ($arr as $val) {
         $path=__DIR__."/../$val/$class.php";
@@ -11,51 +10,34 @@ spl_autoload_register(/**
     }
 });
 
-/**
- * Class Author
- */
+
 class Author
 {
 
     private $id;
-    /**
-     * @var
-     */
+    
     private $name;
-    /**
-     * @var
-     */
+  
     private $country;
 
-    /**
-     * @return mixed
-     */
+    
     public function getName ()
     {
         return $this->name;
     }
 
-    /**
-     * @return mixed
-     */
+   
     public function getCountry ()
     {
         return $this->country;
     }
 
-    /**
-     * @return null
-     */
     public function getId ()
     {
         return $this->id;
     }
 
-    /**
-     * Author constructor.
-     * @param $name
-     * @param $country
-     */
+  
     public function __construct($name,$id=null, $country=null)
     {
         $this->name = $name;
