21c774f901f50bb9c5403ffe41c8020fcea345e8
Abhishek Sharma <102028325+Abhi2003shek@users.noreply.github.com>
2023-02-16 18:38:54 +0530

    Update Admin.php

--- models/admin/Admin.php
+++ models/admin/Admin.php
@@ -1,11 +1,5 @@
 <?php
-/**
- * this bage  coded  by Ahmed  Embaby in  24  SEP  2019
- */
 
-/**
- * this page  coded  by Ahmed  Embaby in  29  SEP  2019
- */
 
 class Admin
 {
@@ -14,12 +8,7 @@ class Admin
     private $pass;
     private static $admins=[];
 
-    /**
-     * Admin constructor.
-     * @param $id
-     * @param $name
-     * @param $pass
-     */
+   
     public function __construct ($id, $name, $pass)
     {
         $this->id = $id;
@@ -29,25 +18,18 @@ class Admin
 
     }
 
-    /**
-     * @return mixed
-     */
     public function getId ()
     {
         return $this->id;
     }
 
-    /**
-     * @return mixed
-     */
+  
     public function getName ()
     {
         return $this->name;
     }
 
-    /**
-     * @return mixed
-     */
+  
     public function getPass ()
     {
         return $this->pass;
