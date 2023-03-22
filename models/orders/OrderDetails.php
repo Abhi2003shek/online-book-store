c59bbbbb3ecf3851d781ca9f77540e3361627e97
Abhishek Sharma <102028325+Abhi2003shek@users.noreply.github.com>
2023-03-22 19:11:32 +0530

    Update OrderDetails.php

--- models/orders/OrderDetails.php
+++ models/orders/OrderDetails.php
@@ -1,8 +1,7 @@
 <?php
 
-spl_autoload_register(/**
- * @param $class
- */ function ($class){
+spl_autoload_register(
+ function ($class){
     $arr=['goods','interfaces','orders','reviews','serve','customer'];
     foreach ($arr as $val) {
         $path=__DIR__."/../$val/$class.php";
@@ -11,25 +10,15 @@ spl_autoload_register(/**
     }
 });
 
-/**
- * Class OrderDetails
- */
+
 class OrderDetails extends Review implements Damage
 {
-    /**
-     * @var
-     */
+    
     private $book;
-    /**
-     * @var
-     */
+    
     private $quantity;
 
-    /**
-     * OrderDetails constructor.
-     * @param $book
-     * @param $quantity
-     */
+    
     public function __construct ($book, $quantity,$openion=NULL,$reviewDegree=NULL)
     {
         parent::__construct($openion,$reviewDegree);
@@ -38,48 +27,36 @@ class OrderDetails extends Review implements Damage
     }
 
 
-    /**
-     * @return mixed
-     */
+    
     public function getBook():Book
     {
         return $this->book;
     }
 
-    /**
-     * @return mixed
-     */
+    
     public function getQuantity()
     {
         return $this->quantity;
     }
 
-    /**
-     * @param mixed $quantity
-     */
+    
     public function setQuantity($quantity): void
     {
         $this->quantity = $quantity;
     }
 
-    /**
-     * @param int $quantity
-     */
+    
     public function addQuantity(int $quantity){
         $this->quantity+=$quantity;
     }
 
-    /**
-     * @param mixed $book
-     */
+    
     public function setBook($book): void
     {
         $this->book = $book;
     }
 
-    /**
-     *
-     */
+    
     public function damageAllData()
     {
         unset(
